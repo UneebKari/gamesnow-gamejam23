4faaafda2f533f839ad6d2a2dab2e860c39fe0c2
DarkMeadow322 <jesparz@iu.edu>
2023-11-09 16:43:26 -0500

    Paralaxing done?
    I added several layers that can be easily manipulated and replaced. Some only work on button press.

---
+++ Assets/Scripts/RotateOnPress.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class RotateOnPress : MonoBehaviour
+{
+    public float rotationSpeed = 90.0f; // Rotation speed in degrees per second
+
+    private Material material;
+    private Vector2 offset;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = material.mainTextureOffset;
+    }
+
+    void Update()
+    {
+        //Check if a key is being pressed
+        if (Input.anyKey)
+        {
+            // Calculate the new offset to rotate the texture
+            offset.x += rotationSpeed * Time.deltaTime;
+
+            // Make sure the offset stays within the [0, 1] range to avoid issues
+            offset.x %= 1.0f;
+
+            // Update the material's main texture offset
+            material.mainTextureOffset = offset;
+        }
+    }
+}
